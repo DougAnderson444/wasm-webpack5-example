12e325b3f8a830637f5e2a5ccca8e558d4c74ceb
Doug <douganderson444@gmail.com>
2021-03-11 13:05:08 -0500

    add a DOM message 😍

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -9,7 +8,6 @@ use web_sys::console;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -19,9 +17,20 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
+
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello example from Rust!"));
+
+    body.append_child(&val)?;
 
     Ok(())
 }
